4324846a77ef6c070f9c9ff7bb2365e944b9433e
Oded <odedhb@gmail.com>
2019-08-04 10:12:26 +0300

    Get stackoverflow results

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -11,15 +11,39 @@ chrome.extension.sendMessage({}, function (response) {
 
             function getSearchResults() {
                 var htmlResults = document.getElementsByClassName("LC20lb");
-                console.log(htmlResults);
+                // console.log(htmlResults); //for debugging
                 for (var result of htmlResults) {
                     if (result.innerText.indexOf(' - npm') != -1) {
-                        getPackageData(result);
+                        getNpmData(result);
+                    } else if (result.innerText.indexOf(' - Stack Overflow') != -1) {
+                        getStackOverflowData(result);
                     }
                 }
             }
 
-            function getPackageData(result) {
+            function getStackOverflowData(result) {
+                var questionID = result.nextSibling.nextElementSibling.innerText.split('stackoverflow.com/questions/').pop().split('/')[0];;
+                fetch('https://api.stackexchange.com/2.2/questions/' + questionID + '/answers?&site=stackoverflow&filter=withbody&sort=votes')
+                    .then(
+                        function (response) {
+                            if (response.status !== 200) {
+                                console.log('Looks like there was a problem. Status Code: ' +
+                                    response.status);
+                                return;
+                            }
+
+                            // Manipulate the text in the response
+                            response.json().then(function (data) {
+                                result.innerHTML = result.innerHTML + '<span style="display: inline-block; text-decoration:none !important; color: dimgrey; font-size: small;">&nbsp-&nbsp' + data.items[0].body + '</span>';
+                            });
+                        }
+                    )
+                    .catch(function (err) {
+                        console.log('Fetch Error :-S', err);
+                    });
+            }
+
+            function getNpmData(result) {
                 var packageName = result.innerText.replace(' - npm', '');
                 fetch('https://api.npmjs.org/downloads/point/last-week/' + packageName)
                     .then(
@@ -30,26 +54,17 @@ chrome.extension.sendMessage({}, function (response) {
                                 return;
                             }
 
-                            // Examine the text in the response
+                            // Manipulate the text in the response
                             response.json().then(function (data) {
-                                injectStats(result, data);
+                                result.innerHTML = result.innerHTML + '<span style="display: inline-block; text-decoration:none !important; color: dimgrey; font-size: small;">&nbsp-&nbsp' + data.downloads.toLocaleString() + ' weekly downloads</span>';
                             });
                         }
                     )
                     .catch(function (err) {
                         console.log('Fetch Error :-S', err);
                     });
-
-                // ----------------------------------------------------------
-
             }
 
-            function injectStats(result, data) {
-                // result.innerText = result.innerText + ' (' + data.downloads.toLocaleString() + ' weekly downloads)';
-
-
-                result.innerHTML = result.innerHTML + '<span style="display: inline-block; text-decoration:none !important; color: dimgrey; font-size: small;">&nbsp-&nbsp' + data.downloads.toLocaleString() + ' weekly downloads</span>';
-            }
 
         }
     }, 10);
