d93275d8e64a7558fe32a04347e16ba4ba7b0f03
Oded <odedhb@gmail.com>
2020-04-15 13:19:22 +0300

    analytics

--- src/background/background.js
+++ src/background/background.js
@@ -9,3 +9,13 @@ chrome.runtime.onMessage.addListener(
             return true;  // Will respond asynchronously.
         }
     });
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-162460214-1']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
